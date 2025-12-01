1cbc4f7a0cf16f44cb2a93a42dea7b8d6796d2c7
Wodashin <kevinaguilera97@gmail.com>
2025-12-01 20:25:15 -0300

    Update image configuration with remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,26 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
- 
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-da9cec840809483ea83a2e1dee014a3e.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Para avatares de Google
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // Por si usas placeholders
+      }
+    ],
   },
 }
 
